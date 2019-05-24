7cefe34d2241564af76f38d4e5857a1634bd0fc6
Romildo Jr <fis.romildojr@gmail.com>
2019-05-24 00:40:23 -0300

    Imprementação da Lista Encadeada Utilizando Ponteiros

--- Estrutura de Dados/Lista/lista.h
+++ Estrutura de Dados/Lista/lista.h
@@ -1,1 +1,24 @@
-Teste de git
+typedef int TipoElemento;
+typedef struct TipoCelula *TipoApontador;
+
+typedef struct {
+	TipoElemento Elemento;
+} TipoItem;
+
+typedef struct TipoCelula {
+	TipoItem Item;
+	TipoApontador Prox;
+} TipoCelula;
+
+typedef struct {
+	TipoApontador Inicio;
+	TipoApontador Fim;
+}TipoLista;
+
+void FLVazia(TipoLista *Lista);
+
+int Vazia(TipoLista Lista);
+
+void Inserir(TipoLista *Lista, TipoItem x);
+
+void Imprimir(TipoLista Lista);
