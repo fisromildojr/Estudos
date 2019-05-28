e9190eecd72360e7954ddc91f64ebadd89791687
Romildo Jr <fis.romildojr@gmail.com>
2019-05-28 14:16:49 -0300

    Implementação de Pilha com Apontadores terminada (Funcionando)

--- Estrutura de Dados/Pilha/Apontadores/main.c
+++ Estrutura de Dados/Pilha/Apontadores/main.c
@@ -2,11 +2,36 @@
 #include <stdlib.h>
 #include "pilha.h"
 
-int main(){
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
 	TipoPilha Pilha;
 	TipoItem x;
 	
 	FPVazia(&Pilha);
+	
+	x.Chave=2;
+	Empilhar(&Pilha, x);
+	
+	x.Chave=3;
+	Empilhar(&Pilha, x);
+	
+	x.Chave=5;
+	Empilhar(&Pilha, x);
+	
+	if(Vazia(Pilha)){
+		printf("A pilha esta vazia...");
+	}
+	else{
+		printf("O tamanho da pilha e de %d itens...\n", Pilha.Tamanho);
+		Imprimir(Pilha);
+	}
+	
+	Desempilhar(&Pilha, &x);
+	
+	printf("\nO Item desempilhado foi %d ...\n", x.Chave);
+	
+	Imprimir(Pilha);
 	
 	return 0;
 }
--- Estrutura de Dados/Pilha/Apontadores/pilha.c
+++ Estrutura de Dados/Pilha/Apontadores/pilha.c
@@ -3,10 +3,39 @@
 #include "pilha.h"
 
 void FPVazia(TipoPilha *Pilha){
-	Pilha->Topo = (TipoApontador) malloc(sizeof(TipoItem));
+	Pilha->Topo = (TipoApontador) malloc(sizeof(TipoCelula));
+	Pilha->Base = Pilha->Topo;
 	Pilha->Topo->Prox = NULL;
+	Pilha->Tamanho = 0;
 }
 
 int Vazia(TipoPilha Pilha){
-	return (Pilha->Topo->Prox == -1);
+	return (Pilha.Topo == Pilha.Base);
+}
+
+void Empilhar(TipoPilha *Pilha, TipoItem x){
+	TipoApontador Aux;
+	Aux = (TipoApontador) malloc(sizeof(TipoCelula));
+	Aux->Prox = Pilha->Topo;
+	Pilha->Topo->Item = x;
+	Pilha->Topo = Aux;
+	Pilha->Tamanho++;
+}
+
+void Desempilhar(TipoPilha *Pilha, TipoItem *x){
+	TipoApontador Aux;
+	*x = Pilha->Topo->Prox->Item;
+	Aux = Pilha->Topo;
+	Pilha->Topo = Aux->Prox;
+	free(Aux);
+	Pilha->Tamanho--;
+}
+
+void Imprimir(TipoPilha Pilha){
+	TipoApontador Aux;
+	Aux = Pilha.Topo;
+	while( Aux != Pilha.Base){
+		printf("%d\n", Aux->Prox->Item.Chave);
+		Aux = Aux->Prox;
+	}
 }
